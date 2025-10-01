edc43d168fe3fc2bb136228cbc9f7f9a1028da37
Fabiha188 <fabihasiddiqui188@gmail.com>
2025-10-01 12:46:59 -0700

    Update Question5.cpp

--- 24k-1022_LAB05/Question5.cpp
+++ 24k-1022_LAB05/Question5.cpp
@@ -1,23 +1,71 @@
 #include <iostream>
 using namespace std;
+ 
+int totalCount=0;
 
-int sumDigits(int n) {
-    if (n == 0) return 0;
-    return (n % 10) + sumDigits(n / 10);
+void print(int board[20][20],int N){
+    for(int i=0;i<N ;i++){
+        for(int j=0;j<N;j++){
+            if(board[i][j]==1){
+                cout<<" Q ";
+
+            }
+            else{
+                cout<<" . ";
+            }
+            
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+}
+bool isSafe(int board[20][20],int N,int col,int row){
+    for(int r=0;r<row;r++){
+        if(board[r][col]==1){
+            return false;
+        }
+    }
+    for(int i=row,j=col;i>=0 && j>=0;i--,j--){
+        if(board[i][j]==1){
+            return false;
+        }
+    }
+    for(int i=row,j=col;i>=0 && j<N;i--,j++){
+        if(board[i][j]==1){
+            return false;
+        }
+    }
+    return true;
 }
 
-int nestedSum(int n) {
-    if (n < 10) return n;
-    return nestedSum(sumDigits(n));
+bool solveNQueen(int board[20][20],int N,int row){
+    if(row==N){
+        totalCount++;
+        cout<<"Solution Count: "<<totalCount<<endl;
+        print(board,N);
+        return true;
+    }
+bool result=false;
+    for(int i=0;i<N;i++){
+        if(isSafe(board,N,i,row)){
+            board[row][i]=1;
+            result=solveNQueen(board,N,row+1) || result;
+            board[row][i]=0;
+        }
+    }
+return result;
 }
-
-int main() {
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
-
-    int result = nestedSum(n);
-    cout << "Final single-digit result: " << result << endl;
+int main(){
+    int N;
+    cout<<"Enter Value Of N: ";
+    cin>>N;
+    int board[20][20]={0};
+    if(!solveNQueen(board,N,0)){
+        cout<<"No Solution Found";
+    }
+    else{
+        cout << "Total number of distinct solutions for N=" << N << " is: " << totalCount << endl;
+    }
 
     return 0;
 }
